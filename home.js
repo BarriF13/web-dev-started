117eb9dd98f67d117764290ae3a9bf15f48df067
Barri <info@bdesignstudio.co.uk>
2020-04-24 11:04:33 +0100

    functions

--- home.js
+++ home.js
@@ -1,5 +1,10 @@
-const price = "bla";
-// let times_99= 99;
+// function logMessage(){
+//   console.log('Here is a message')
+// }
+// logMessage(); function declaration
 
+// let myFunction = function logging(){//we can name the function as well but can't be called
+//   console.log('Here is expression')
+// };
+// myFunction();
 
- showMessage(price);
