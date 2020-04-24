bb388b5a703510eba27a71851fe759e2b5b2e78b
Barri <info@bdesignstudio.co.uk>
2020-04-24 11:41:51 +0100

    Function Scope

--- home.js
+++ home.js
@@ -15,9 +15,39 @@
 // }
 // showMessage('First message', 'how is it going');
 
-//---
-let myFunction = function logging(message, name) {
-  console.log(message, name);
+//---Send info into the function
+// let myFunction = function logging(message, name) {
+//   console.log(message, name);
 
-};
-myFunction('Hey coder...','Barri');
+// };
+// myFunction('Hey coder...','Barri');
+
+//---Get information out of the function
+
+// function getSecretCode(value) {
+//   // let code = value +3;
+//   return value + 3
+// };
+// console.log(getSecretCode(34));
+
+//--cleaner version
+// let secretCode = function getSecretCode(value) {
+//   return value *32;
+// };
+// showMessage(secretCode(2));
+
+//---scope
+var key = 42;
+function getSecretCode(value){
+  let keyGen = function(){
+    let key = 12;
+    console.log('in keygen: ', key);
+    return key;
+  }
+ 
+  let code = value * keyGen();
+  console.log(' in get secret code', key);
+  return code
+}
+let secretCode = getSecretCode(2);
+showMessage(secretCode);
